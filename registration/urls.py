947c438518af0857ef48e90a6610ad560d27bd55
Guruju Ganesh Sri Karthikeya <109730373+karthikeyaguruju@users.noreply.github.com>
2023-09-30 23:39:40 +0530

    Update urls.py

--- registration/urls.py
+++ registration/urls.py
@@ -18,10 +18,7 @@ from django.urls import path
 from app1 import views
 urlpatterns = [
     path('ganesh/', admin.site.urls),
-    path('',views.SignupPage,name='signup'),
-    path('login/',views.LoginPage,name='login'),
-    path('home/',views.HomePage,name='home'),
-    path('logout/',views.LogoutPage,name='logout'),
+    path('',views.HomePage,name='home'),
 
 
     
